b53662c2da888d23394666541f93e608c9ec908e
andrea.nvr <an.nvr@gmail.com>
2023-11-14 21:49:05 +1000

    Final updates:
    tracking the number of dice rolls,
    tracking the time it took to win,
    saving the best time to local storage

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,78 +3,114 @@ import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 import Die from "./Die"
 
-
-function App() {
-/* state here */
-const [dice, setDice] = useState(allNewDices())
-const [tenzies, setTenzies] = useState(false)
-
-/* effects here */
-useEffect(() => {
-  const allHeld = dice.every(die => die.isHeld)
-  const arbitraryValue = dice[0].value
-  const allSameValue = dice.every(die => die.value === arbitraryValue)
-
-  if (allHeld && allSameValue) {setTenzies(true) }
-}, [dice])
-
-  /* functionalities here */
-  function allNewDices() {
-    const diceArray = []
-    for (let i = 0; i < 10; i++) {
-      diceArray.push({
-        id: nanoid(),
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false
-      })
+export default function App() {
+  /* state here */
+  const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+  const [rollCount, setRollCount] = useState(0)
+  const [timer, setTimer] = useState(0)
+  const [timerCollection, setTimerCollection] = useState([])
+  const [bestTime, setBestTime] = useState(
+    parseInt(localStorage.getItem("bestTime"), 10) || null
+  );
+  
+  /* effects here */
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const arbitraryValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === arbitraryValue)
+  
+    if (allHeld && allSameValue) {
+      setTenzies(true)
     }
-    return diceArray
-  }
-
-  function rollDice() {
-    if (!tenzies) {
-      setDice(prevState => prevState.map(item => {
-        if (!item.isHeld) {
-          return {...item, value: Math.ceil(Math.random() * 6)}
-        }
-        return item
-      }))
+  }, [dice])
+  
+  useEffect(() => {
+    let interval
+  
+    if(tenzies) {
+      clearInterval(interval)
     } else {
-      setTenzies(false)
-      setDice(allNewDices())
+      interval = setInterval(() => {
+        setTimer(prevTimer => prevTimer + 1)
+      }, 1000)
     }
-  }
-
-  function holdDice(id) {
-    setDice(prevState => prevState.map(item => {
-        if (item.id === id) {
-            return {...item, isHeld: !item.isHeld}
-        }
-        return item
-      })
+  
+    return () => clearInterval(interval) 
+  }, [tenzies])
+  
+  useEffect(() => {
+    if (tenzies) {
+      // Game won, update timerCollection
+      setTimerCollection((prevTimer) => [...prevTimer, timer]);
+      // Update best time
+      const minTime = Math.min(...timerCollection, timer);
+      setBestTime(minTime);
+      // Store in localStorage
+      localStorage.setItem("bestTime", minTime.toString());
+    }
+  }, [tenzies, timer, timerCollection])
+  
+  
+    /* functionalities here */
+    function allNewDice() {
+      const diceArray = []
+      for (let i = 0; i < 10; i++) {
+        diceArray.push({
+          id: nanoid(),
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false
+        })
+      }
+      return diceArray
+    }
+  
+    function rollDice() {
+      if (!tenzies) {
+        setDice(prevState => prevState.map(item => {
+          if (!item.isHeld) {
+            return {...item, value: Math.ceil(Math.random() * 6)}
+          }
+          return item
+        }))
+        setRollCount(prevCount => prevCount + 1)
+      } else {
+        setTenzies(false)
+        setDice(allNewDice())
+        setRollCount(0)
+        setTimer(0)
+      }
+    }
+  
+    function holdDice(id) {
+      setDice(prevState => prevState.map(item => {
+          if (item.id === id) {
+              return {...item, isHeld: !item.isHeld}
+          }
+          return item
+        })
+      )
+    }
+  
+    const diceElements = dice.map(item => (
+      <Die
+        key={item.id}
+        value={item.value}
+        isHeld={item.isHeld}
+        holdDice={() => holdDice(item.id)}
+        />))
+  
+    return (
+      <main>
+        {tenzies && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. 
+        Click each die to freeze it at its current value between rolls.</p>
+        <p className="rolls">Rolls: {rollCount}</p>
+        <p className="time">Time: {timer} seconds</p>
+        <p className="best-time">Best Time: {bestTime || "N/A"} seconds</p>      
+        <div className="container">{diceElements}</div>
+        <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+      </main>
     )
   }
-
-  const diceElements = dice.map(item => (
-    <Die
-      key={item.id}
-      value={item.value}
-      isHeld={item.isHeld}
-      holdDice={() => holdDice(item.id)}
-      />))
-
-  return (
-    <main>
-      {tenzies && <Confetti />}
-      <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. 
-      Click each die to freeze it at its current value between rolls.</p>      
-      <div className="container">
-      {diceElements}
-      </div>
-      <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
-    </main>
-  )
-}
-
-export default App
--- app/src/Die.jsx
+++ app/src/Die.jsx
@@ -2,6 +2,7 @@ export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
     }
+
     return (
         <div className="die" style={styles} onClick={props.holdDice}>
             <h2 className="num">{props.value}</h2>
